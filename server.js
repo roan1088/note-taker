cd4aefb2c6efbc47dc9742ef0e406cd51d5e1ed3
roan1088 <rohan_188@yahoo.com>
2020-10-29 17:13:46 -0400

    Added serving static files from the public folder

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
+// For static files, look in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 // HTML Routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
