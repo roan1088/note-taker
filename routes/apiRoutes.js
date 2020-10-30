7e943b15a007f9084520a4d41f3987e2aec9a1d8
roan1088 <rohan_188@yahoo.com>
2020-10-29 20:00:32 -0400

    Added api route to get notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,5 +3,7 @@ var db = require("../db/db.json");
 
 // Exporting a function that creates api routes
 module.exports = function(app) {
-    
+    app.get("/api/notes", function(req, res) {
+        res.json(db);
+      });    
 };
