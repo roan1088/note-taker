f11c3878ca2f2f7b8a412ea5cb41d154f9af42f1
roan1088 <rohan_188@yahoo.com>
2020-10-29 22:10:13 -0400

    Added post method to save notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,26 @@ var db = require("../db/db.json");
 
 // Exporting a function that creates api routes
 module.exports = function(app) {
+    // GET request
     app.get("/api/notes", function(req, res) {
         res.json(db);
-      });    
+    });
+    // POST request
+    app.post("/api/notes", function(req, res) {
+        // Set id to 1 incase there is no notes in the db
+        let id = 1;
+        // If there are notes, get the id of the last note and add 1 to it
+        if (db.length > 0) {
+            const lastNoteId = db[db.length - 1].id;
+            id = lastNoteId + 1;
+        }
+        // Get the note from request body
+        const newNote = req.body;
+        // Give it the new id
+        newNote.id = id;
+        // Push the new note to the db
+        db.push(newNote);
+        // Return the new note
+        res.json(newNote);
+    });
 };
